6c0df968b7c639720d84e6fe3a999244fb99a9d8
Muhammad Hamza <mr.hamxa942@gmail.com>
2025-08-15 15:14:36 +0500

    dvc pipeline completed

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -12,7 +12,7 @@ logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler('errors.log')
+file_handler = logging.FileHandler('logs/errors.log')
 file_handler.setLevel(logging.ERROR)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
