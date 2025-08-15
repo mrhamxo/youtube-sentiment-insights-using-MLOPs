59bb8dbcbd84a8b747b7252b812d20186721fa20
Muhammad Hamza <mr.hamxa942@gmail.com>
2025-08-15 12:56:14 +0500

    files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Hamza',
+    author_email= 'mr.hamxa942@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
